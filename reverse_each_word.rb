5575da9fa7344c516d5aa2a080e2d6c6b6bc2ed6
Oana Berentan <oana.berentan@yahoo.com>
2019-05-06 14:43:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,10 @@
-phrase = "Hello there, and how are you?"
+string = "Hello there, and how are you?"
 
-def reverse_each_word(phrase)
-  puts "phrase"
+def reverse_each_word(string)
+original_array = string.split(" ")
+ return_array = []
+original_array. each do |string|
+  return_array << string.reverse
+end
+  return_array.join(" ")
 end
