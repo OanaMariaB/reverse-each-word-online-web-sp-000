129ac677f1898ff4a9d8087a98a7dedd007a51ca
Oana Berentan <oana.berentan@yahoo.com>
2019-05-06 14:48:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,18 @@
 string = "Hello there, and how are you?"
 
 def reverse_each_word(string)
-original_array = string.split(" ")
- return_array = []
-original_array. each do |string|
+  original_array = string.split(" ")
+    return_array = []
+    original_array. each do |string|
   return_array << string.reverse
 end
   return_array.join(" ")
+end
+
+def reverse_each_word(string)
+    array = string.split(" ")
+    reversed_array = []
+      array.collect do |string|
+    reversed_array << string.reverse
+  end
 end
