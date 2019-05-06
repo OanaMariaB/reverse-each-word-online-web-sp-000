a640be7486e5010c65b4317e7e66eca65a883456
Oana Berentan <oana.berentan@yahoo.com>
2019-05-06 14:02:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 phrase = "Hello there, and how are you?"
 
 def reverse_each_word(phrase)
-  phrase_to_array = phrase.split
   phrase.each do |phrase|
-    puts phrase_to_array.reverse
+    puts phrase.split.reverse
   end
 end
