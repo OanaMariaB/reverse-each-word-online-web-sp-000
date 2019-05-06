e590b86b2c822360b399b9751abdfae8110b53f2
Oana Berentan <oana.berentan@yahoo.com>
2019-05-06 14:49:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,9 +10,9 @@ end
 end
 
 def reverse_each_word(string)
-  array = string.split(" ") 
+  array = string.split(" ")
   test_array = []
-  array.collect do|string| 
+  array.collect do|string|
     test_array << string.reverse 
   end
   test_array.join(" ")
